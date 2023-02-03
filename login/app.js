4b72a03c89f4ed97d4e3a772a98da8599e1ec05d
khubclc <123618031+khubclc@users.noreply.github.com>
2023-02-03 10:28:17 +0800

    Support error messages
    Can now handle error messages sent back by K-Hub Server

--- login/app.js
+++ login/app.js
@@ -1,14 +1,14 @@
 var icon = document.getElementById("icon");
 
-  icon.onclick = function(){
+icon.onclick = function () {
   document.body.classList.toggle("dark-mode");
-  if(document.body.classList.contains("dark-mode")){
-  icon.src = "images/sun.png";
-  }else{
-  icon.src = "moon-solid.svg";
+  if (document.body.classList.contains("dark-mode")) {
+    icon.src = "images/sun.png";
+  } else {
+    icon.src = "moon-solid.svg";
   }
-  };
-  const sign_in_btn = document.querySelector("#sign-in-btn");
+};
+const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
@@ -20,3 +20,13 @@ sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
 
+const urlString = window.location.href
+const paramString = urlString.split('?')[1]
+const searchParams = new URLSearchParams(paramString)
+
+if (searchParams.has("errorcode")) {
+  if(searchParams.get("errorcode") === '3') {
+    const errorElement = document.getElementById("error")
+    errorElement.classList.remove("hidden")
+  }
+}
